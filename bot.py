3e26298105de8bd5a755db01146215f08093ac5d
golujikullu <102353476+golujikullu@users.noreply.github.com>
2026-05-21 19:05:47 +0530

    Update bot.py
    final fix

--- bot.py
+++ bot.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+
 @app.route("/")
 def home():
     return "Rocket Hunter Running 🚀"
 
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
 
-    data = request.get_json()
-
-    print(data)
+    data = request.get_json(force=True)
 
     if "message" in data:
 
         chat_id = data["message"]["chat"]["id"]
 
-        text = data["message"].get("text")
+        text = data["message"].get("text", "")
 
         if text == "/start":
 
@@ -29,7 +29,7 @@ def webhook():
 
             payload = {
                 "chat_id": chat_id,
-                "text": "🚀 Rocket Hunter Activated!"
+                "text": "🚀 Rocket Hunter Activated"
             }
 
             requests.post(url, json=payload)
