32651706a0527cd274c74939c84f6b4eeb0b833c
golujikullu <102353476+golujikullu@users.noreply.github.com>
2026-06-23 11:26:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1803,7 +1803,7 @@ START ENGINE
 
 ==========================================
 if __name__ == "__main__":
-
+send_discord_alert("🚀 Captain Hook Test Message")
     Thread(target=scanner, daemon=True).start()
     Thread(target=outcome_tracker, daemon=True).start()
 
